18e50dc4815828caca9fd7ff3e1d1342dafea486
Rama Firmansyah <ramasatriafb@gmail.com>
2019-10-24 23:44:14 +0700

    Detect Active Menu in Vue Router and Laravel History mode

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -80,7 +80,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
         
           <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+            <a href="#" class="nav-link">
               <i class="nav-icon fas fa-cog"></i>
               <p>
                 Management
@@ -89,7 +89,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Active Page</p>
                 </a>
