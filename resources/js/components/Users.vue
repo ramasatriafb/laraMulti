45189cf5bdb882e0692e2fe406aacdf3f3fb23e6
Rama Firmansyah <ramasatriafb@gmail.com>
2019-10-25 17:42:51 +0700

    Using vForm for Backend Error Handling Laravel Vue

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -7,7 +7,10 @@
                 <h3 class="card-title">Users Table</h3>
 
                 <div class="card-tools">
-                    <button class="btn btn-success" >Add New <i class="fas fa-user-plus fa-fw"></i></button>
+                  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#addNew">
+                      Add New
+                      <i class="fas fa-user-plus fa-fw"></i>
+                    </button>
                 </div>
               </div>
               <!-- /.card-header -->
@@ -46,7 +49,73 @@
             <!-- /.card -->
           </div>
         </div>
+        <!-- Modal -->
+            <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"  id="addNewLabel">Add New</h5>
+                    <h5 class="modal-title" id="addNewLabel">Update User's Info</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form>
+                <div class="modal-body">
+                     <div class="form-group">
+                        <input v-model="form.name" type="text" name="name"
+                            placeholder="Name"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                        <has-error :form="form" field="name"></has-error>
+                    </div>
+
+                     <div class="form-group">
+                        <input v-model="form.email" type="email" name="email"
+                            placeholder="Email Address"
+                            class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                        <has-error :form="form" field="email"></has-error>
+                    </div>
+
+                     <div class="form-group">
+                        <textarea v-model="form.bio" name="bio" id="bio"
+                        placeholder="Short bio for user (Optional)"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                        <has-error :form="form" field="bio"></has-error>
+                    </div>
+
+
+                    <div class="form-group">
+                        <select name="type" v-model="form.type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                            <option value="">Select User Role</option>
+                            <option value="admin">Admin</option>
+                            <option value="user">Standard User</option>
+                            <option value="author">Author</option>
+                        </select>
+                        <has-error :form="form" field="type"></has-error>
+                    </div>
+
+                    <div class="form-group">
+                        <input v-model="form.password" type="password" name="password" id="password"
+                        class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                        <has-error :form="form" field="password"></has-error>
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Update</button>
+                    <button type="submit" class="btn btn-primary">Create</button>
+                </div>
+
+                </form>
+
+                </div>
+            </div>
+            </div>
     </div>
+    </div>
+
+    
 </template>
 
 <script>
@@ -54,15 +123,15 @@
         data() {
             return {
                 users : {},
-                // form: new Form({
-                //     id:'',
-                //     name : '',
-                //     email: '',
-                //     password: '', 
-                //     type: '',
-                //     bio: '',
-                //     photo: ''
-                // })
+                form: new Form({
+                    id:'',
+                    name : '',
+                    email: '',  
+                    password: '', 
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
             }
         },
         methods: {
