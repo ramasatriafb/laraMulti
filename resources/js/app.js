18a32655dabb7bbf0c42e67b8e4b5c364d196b0b
Rama Firmansyah <ramasatriafb@gmail.com>
2019-10-30 00:49:51 +0700

    Page 404 in Frontend if User doesnt have access

--- resources/js/app.js
+++ resources/js/app.js
@@ -90,6 +90,10 @@ Vue.component(
     require('./components/passport/PersonalAccessTokens.vue').default
 );
  
+Vue.component(
+    'not-found',
+    require('./components/NotFound.vue').default
+);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -58,10 +58,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="./img/user.png" class="img-circle elevation-2" alt="User Image">
+          <img src="./img/profile/{{ Auth::user()->photo }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+          <a href="#" class="d-block">
+            {{ Auth::user()->name }}
+            <p>{{ Auth::user()->type }}</p>
+          </a>
         </div>
       </div>
 
